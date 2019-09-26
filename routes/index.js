3f732c843ef9088aee7349ce2db56c3ded6d47d7
ssudh <skukutla@horizon.csueastbay.edu>
2019-09-26 16:52:11 -0700

    First version

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var app = express.Router();
+var mongodb= require('mongodb');
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://heroku_s1f77zf9:f6rd1i104os5evk26pkhbg3dn8@ds147125.mlab.com:47125/heroku_s1f77zf9';
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
+
+//**************************
+//*** mongodb get all of the Routes in Routes collection where frequence>=1
+//      and sort by the name of the route.  Render information in the views/pages/mongodb.ejs
+router.get('/mongodb', function (request, response) {
+
+  MongoClient.connect(url, function(err, db) {
+    console.log(db);
+    if(err) throw err;
+    //get collection of routes
+    var Routes = db.db('heroku_s1f77zf9').collection('Routes');
+    console.log(Routes)
+    //get all Routes with frequency >=1
+    Routes.find({ frequency : { $gte: '1' } }).sort({ name: 1 }).toArray(function (err, docs) {
+      if(err) throw err;
+
+      response.render('pages/mongodb', {results: docs});
+
+    });
+
+    //close connection when your app is terminating.
+    db.close(function (err) {
+      if(err) throw err;
+    });
+  });//end of connect
+});//end app.get
